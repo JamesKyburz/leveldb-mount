dc684f7dc11e8c37b4e16aba6eae4f89c76f5fed
James Kyburz <james.kyburz@gmail.com>
2016-04-15 08:52:40 +0200

    export db, routes, server with same options

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ var server = require('./server')
 var routes = require('./routes')
 var db = require('./db')
 
-exports.server = server
-exports.db = db
-exports.routes = routes
+module.exports = mount
+
+mount.server = server
+mount.db = db
+mount.routes = routes
+
+function mount (opt) {
+  return {
+    server (server, name) {
+      return mount.server(server, name, opt)
+    },
+    db (name) {
+      return mount.db(name, opt)
+    },
+    routes (name) {
+      return mount.routes(name, opt)
+    }
+  }
+}
