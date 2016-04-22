d75b245a05730110c01fc4b8a4d4acd501f9d589
James Kyburz <james.kyburz@gmail.com>
2016-04-22 19:15:33 +0200

    playing with prototypes

--- repl.js
+++ repl.js
@@ -4,6 +4,7 @@ var sub = require('subleveldown')
 var levelup = require('levelup')
 var memdown = require('memdown')
 var opt = 'replacedbyserver'
+var EventEmitter = require('events')
 
 ;(function connect () {
   var protocol = window.location.protocol === 'https:' ? 'wss' : 'ws'
@@ -15,6 +16,17 @@ var opt = 'replacedbyserver'
     valueEncoding: opt.valueEncoding,
     db: memdown
   })
+
+  var changes = onChanges(cacheDb)
+  changes.on('change', (key, value) => {
+    console.log('change! %s:%s', key, JSON.stringify(value))
+  })
+  setInterval(function () {
+    onChanges(cacheDb, changes)
+  }, 1000)
+
+  cacheDb.put('testing', 100)
+
   var db = multileveldown.server(cacheDb)
   var remoteDb = multileveldown.client(opt)
   var remote = remoteDb.connect()
@@ -32,3 +44,26 @@ var opt = 'replacedbyserver'
   window.db = cacheDb
   window.remote = remoteDb
 })()
+
+function onChanges (db, ee) {
+  ee = ee || new EventEmitter()
+  if (!db.db._put.hijacked) {
+    var put = db.db._put.bind(db.db)
+    db.db._put = function (key, value, opt, cb) {
+      if (!opt && !cb) cb = function () {}
+      function done (cb) {
+        return function (err) {
+          if (!err) ee.emit('change', key, value)
+          cb(err)
+        }
+      }
+      function wrap (opt) {
+        if (typeof opt === 'function') opt = done(opt)
+        return opt
+      }
+      put(key, value, wrap(opt), wrap(cb))
+    }
+    db.db._put.hijacked = true
+  }
+  return ee
+}
--- server.js
+++ server.js
@@ -1,43 +1,49 @@
 var auth = require('./auth')
 var websocket = require('websocket-stream')
 var multileveldown = require('multileveldown')
+var leveldown = require('multileveldown/leveldown')
 var http = require('http')
 var db = require('./db')
 var options = require('./options')
+var eos = require('end-of-stream')
 var routes = require('./routes')
+var EventEmitter = require('events')
 
 module.exports = create
 
 function create (server, name, opt) {
   opt = options(name, opt)
   if (typeof server === 'number') return create(createServer(server, opt), opt)
+
+  var dbInstance = db(opt)
+  var changes = onChanges(dbInstance)
+
+  changes.on('change', (key, value) => {
+    console.log('change!!!', key, value)
+  })
+
   websocket.createServer({ server: server }, handleWs)
 
   function handleWs (stream) {
     auth(stream.socket.upgradeReq, opt, (error) => {
+      onChanges(dbInstance, changes)
       if (error) {
         stream.socket.emit('error', error)
       } else {
         var remote = multileveldown.client(opt.encoding)
-        var dbInstance = db(opt)
         var local = multileveldown.server(dbInstance)
         var remoteStream = remote.connect()
-        setTimeout(function () {
-          remote.put('test', 10)
-          remote.put('test1', 11)
-          remote.put('test2', 12)
-          remote.put('test3', 13)
-        }, 2000)
+
+        function replicate (key, value) { remote.put(key, value) }
+        changes.on('change', replicate)
+        eos(stream, changes.removeListener.bind(changes, 'change', replicate))
+        dbInstance.put('started', new Date())
 
         remoteStream.on('data', stream.write.bind(stream))
         stream.on('data', remoteStream.write.bind(remoteStream))
 
         local.on('data', stream.write.bind(stream))
         stream.on('data', local.write.bind(local))
-
-        dbInstance.put('started', new Date())
-
-        //stream.pipe(multileveldown.server(db(opt))).pipe(stream)
       }
     })
   }
@@ -55,4 +61,27 @@ function createServer (port, opt) {
     })
   }
   return server
+}
+
+function onChanges (db, ee) {
+  ee = ee || new EventEmitter()
+  if (!db.db._put.hijacked) {
+    var put = db.db._put.bind(db.db)
+    db.db._put = function (key, value, opt, cb) {
+      if (!opt && !cb) cb = function () {}
+      function done (cb) {
+        return function (err) {
+          if (!err) ee.emit('change', key, value)
+          cb(err)
+        }
+      }
+      function wrap (opt) {
+        if (typeof opt === 'function') opt = done(opt)
+        return opt
+      }
+      put(key, value, wrap(opt), wrap(cb))
+    }
+    db.db._put.hijacked = true
+  }
+  return ee
 }
