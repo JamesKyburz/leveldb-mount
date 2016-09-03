5f39fb7b3a315ca9731af592a2fc8b2148f3eebd
James Kyburz <james.kyburz@gmail.com>
2016-09-03 21:37:04 +0200

    client.js isomorphic

--- client.js
+++ client.js
@@ -10,6 +10,16 @@ function client (opt) {
   opt = opt || {}
   opt.keyEncoding = opt.keyEncoding || 'utf8'
   opt.valueEncoding = opt.valueEncoding || 'json'
+
+  var url = opt.url
+
+  if (!url && typeof window !== 'undefined') {
+    var protocol = window.location.protocol === 'https:' ? 'wss' : 'ws'
+    url = `${protocol}://${window.location.host}/db`
+  }
+
+  var retryTimeout = opt.retry || 3000
+
   opt.retry = true
   var cacheKey = opt.keyEncoding + opt.valueEncoding
   var db = dbs[cacheKey]
@@ -19,12 +29,10 @@ function client (opt) {
     db = dbs[cacheKey] = multileveldown.client(opt)
     re = res[cacheKey] = rangeEmitter(db)
     ;(function connect () {
-      var protocol = window.location.protocol === 'https:' ? 'wss' : 'ws'
-      var url = `${protocol}://${window.location.host}/db`
       var ws = websocket(url)
       var remote = db.connect()
       re.session(remote, ws)
-      ws.on('close', window.setTimeout.bind(window, connect, 3000))
+      ws.on('close', window.setTimeout.bind(window, connect, retryTimeout))
     })()
   }
 
